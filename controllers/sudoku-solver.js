f9e1926a1f1c97a73bf3d7120e439884194c3f7b
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-01-14 17:19:51 +0200

    finished post api/check

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,50 @@
 class SudokuSolver {
 
   validate(puzzleString) {
-  }
+    if(puzzleString.length !== 81) return false
+    else return true
+  } 
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    let startRow = row * 9
+    const rowArr = puzzleString.split('').slice(startRow, startRow + 9)
+    if(rowArr[column] !== '.') return false
+    if(rowArr.includes(value)) return false
+    else return true
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    const colArr = []
+    for(let i = column; i < 81; i += 9) {
+      colArr.push(puzzleString[i])
+    }
+    if(colArr[row] !== '.') return false
+    if(colArr.includes('value')) return false
+    else return true
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    const regionArr = []
+    const startRowRegion = (row - (row % 3)) * 9
+    const startColRegion = column - (column % 3)
+    for(let i = startColRegion; i < 27; i+=9) {
+      let result = []
+      for(let j = 0; j < 3;j++) {
+        result.push(puzzleString[i + startRowRegion + j])
+      }
+      regionArr.push(result)
+    }
+    column = column % 3
+    row = row % 3
+    if(regionArr[row][column] !== '.') return false
+    for(let arr of regionArr) {
+      if(arr.includes(value)) return false
+    }
+    return true
   }
 
   solve(puzzleString) {
-    
+    console.log('Solved')
   }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,78 @@ const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
   
-  let solver = new SudokuSolver();
-
+  let Solver = new SudokuSolver();
+  let rowToNumber = {
+    A: 0,
+    B: 1,
+    C: 2,
+    D: 3,
+    E: 4,
+    F: 5,
+    G: 6,
+    H: 7,
+    I: 8
+  }
   app.route('/api/check')
     .post((req, res) => {
+      if(!req.body.value || !req.body.coordinate[0] || !req.body.coordinate[1] || !req.body.puzzle ) {
+        res.json({
+          error: 'Required field(s) missing'
+        });
+        return console.log('Required field(s) missing');
+      }
+      if(req.body.value < 1 || req.body.value > 9) {
+        res.json({
+          error: 'invalid value'
+        });
+        return console.log('invalid number');
 
+      }else if(!Object.keys(rowToNumber).includes(req.body.coordinate[0].toUpperCase())) {
+        res.json({
+          error: 'invalid value'
+        });
+        return console.log('invalid number');
+      }else if(+req.body.coordinate.slice(1) < 1 || +req.body.coordinate.slice(1) > 9) {
+        res.json({
+          error: 'invalid value'
+        });
+        return console.log('invalid number');
+      }
+      let puzzleString = req.body.puzzle 
+      let row = rowToNumber[req.body.coordinate[0].toUpperCase()]
+      let column = req.body.coordinate[1] - 1
+      let value = req.body.value
+      if(!Solver.validate(puzzleString)) {
+        res.json({
+          error: 'Expected puzzle to be 81 characters long'
+        })
+        return console.log('Expected puzzle to be 81 characters long')
+      }
+      let conflict = []
+      let colPlacement = !Solver.checkColPlacement(puzzleString, row, column, value) ? conflict.push('column') : true
+      let regionPlacment = !Solver.checkRegionPlacement(puzzleString, row, column, value) ? conflict.push('region') : true
+      let rowPlacement = !Solver.checkRowPlacement(puzzleString, row, column, value) ? conflict.push('row') : true
+      if(conflict.length > 0) {
+        res.json({
+          valid: false,
+          conflict
+        })
+      }else {
+        res.json({
+          valid: true
+        })
+      }  
     });
     
   app.route('/api/solve')
     .post((req, res) => {
-
+      let puzzleString = req.body.puzzle
+      if(!Solver.validate(puzzleString)) {
+        res.json({
+          error: 'Expected puzzle to be 81 characters long'
+        })
+        return console.log('Expected puzzle to be 81 characters long')
+      }
+      Solver.solve(puzzleString)
     });
 };
